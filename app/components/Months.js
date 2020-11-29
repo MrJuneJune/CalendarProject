3aac6d534a0aea8aabd3f8b3f83d8e4de4ce2cff
Katherine Tran <kathytran@Katherines-MacBook-Pro-2.local>
2020-11-28 19:01:27 -0800

    separated components of calendar into different files

---
+++ app/components/Months.js
@@ -0,0 +1,27 @@
+class Months extends React.PureComponent {
+    renderItem = (item, index) => {
+      const { changeSelectedMonth, selectedMonth } = this.props;
+      const month = index;
+      const cls = classnames("month", {
+        active: month === selectedMonth
+      });
+  
+      return (
+        <div
+          className={cls}
+          key={month}
+          onClick={(e) => changeSelectedMonth(month)}
+        >
+          {item.short}
+        </div>
+      );
+    };
+    render() {
+      return (
+        <section className="months">
+          {MONTHS_CON.map(this.renderItem)}
+          <span className="indicator" />
+        </section>
+      );
+    }
+  }
